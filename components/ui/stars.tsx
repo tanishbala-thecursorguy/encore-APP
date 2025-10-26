0db6ba6f22baa3ea7dcd25ea7be3cf2dd2676120
tanishbala-thecursorguy <tanishbala567@gmail.com>
2025-10-26 19:15:39 -0400

    Change background to fully black instead of green tint

--- components/ui/stars.tsx
+++ components/ui/stars.tsx
@@ -109,7 +109,7 @@ export function StarsBackground({
     <div
       data-slot="stars-background"
       className={cn(
-        "relative size-full overflow-hidden bg-[radial-gradient(ellipse_at_bottom,_#1a3a38_0%,_#000_100%)]",
+        "relative size-full overflow-hidden bg-black",
         className,
       )}
       onMouseMove={handleMouseMove}
